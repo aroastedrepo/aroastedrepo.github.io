015b79388c823790084a1ad6b927b1c6762d873c
sanjanapendharkar01@gmail.com <sanjanapendharkar01@gmail.com>
2025-06-15 10:27:44 -0400

    Changed title

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -30,12 +30,12 @@ const AboutMe = () => {
           </h1>
           <div className="space-y-6">
             <p>
-              I'm Sanjana, an ambitious, driven data scientist at MITRE.
-              Formerly, I was an Associate for their Network Analytics Division,
-              where I led a research study that built an iOS app to help low
-              vision individuals navigate train stations. As a part of this
-              study, I submitted two journal papers on my research. During my
-              tenure here, I also worked on fine-tuning LLMs to support
+              I'm Sanjana, an ambitious, driven data scientist and product
+              designer. Formerly, I was an Associate for their Network Analytics
+              Division, where I led a research study that built an iOS app to
+              help low vision individuals navigate train stations. As a part of
+              this study, I submitted two journal papers on my research. During
+              my tenure here, I also worked on fine-tuning LLMs to support
               warfighters with information acquisition and retrieval.
             </p>
             <p>
